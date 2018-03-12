f6c1ea5bdadbc2bdfeba663b9fef8457330b49a1
siraj <siiraaj@yahoo.fr>
2018-03-12 20:49:35 +0100

    version 1.0

---
+++ WebServiceBoulot/WebServiceBoulot/Employe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceBoulot
+{
+    public class Employe
+    {
+        public int id;
+        public string nom;
+        public string mail;
+        public int telephone;
+    }
+}
