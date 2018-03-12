1268ab99a123ce1ebfc55c38f8be644ee06467a9
siraj <siiraaj@yahoo.fr>
2018-03-12 15:37:13 +0100

    A meaningful message about this change

---
+++ WebServiceBoulot/WebServiceBoulot/Employe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceBoulot
+{
+    public class Employe
+    {
+        public int id;
+        public string nom;
+        public string mail;
+    }
+}
