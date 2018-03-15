b93e1f9f1a02e6b5878c0be36d95f4195b0b9284
siraj <siiraaj@yahoo.fr>
2018-03-15 12:26:26 +0100

    Ajouter fonction GetEmployeJson() qui retourne une sortie enJson

--- WebServiceBoulot/WebServiceBoulot/WebService1.asmx.cs
+++ WebServiceBoulot/WebServiceBoulot/WebService1.asmx.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
 using System.Web.Services;
 
 namespace WebServiceBoulot
@@ -13,14 +15,14 @@ namespace WebServiceBoulot
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+     [System.Web.Script.Services.ScriptService]
     public class WebService1 : System.Web.Services.WebService
     {
 
-        [WebMethod(MessageName = "GetEmploye", Description = "Return contact data from DB")]
+        [WebMethod(MessageName = "GetEmployeXML", Description = "cette methode renvoie XML")]
         [System.Xml.Serialization.XmlInclude(typeof(Employe))]
 
-        public Employe GetEmploye()
+        public Employe GetEmployeXML()
         {
             Employe e = new Employe();
             //e.id = 2;
@@ -30,6 +32,23 @@ namespace WebServiceBoulot
             e.ville = "Marseille";
 
             return e;
+        }
+
+        [WebMethod(MessageName = "GetEmployeJson", Description = "cette methide renvoie Json")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+
+        public string GetEmployeJson()
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            Employe e = new Employe();
+            //e.id = 2;
+            e.nom = "siraj";
+            e.phone = "siiraaj@yahoo.fr";
+            e.profession = "plombier";
+            e.ville = "Marseille";
+
+
+            return ser.Serialize(e);
         }
     }
 }
