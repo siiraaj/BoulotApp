4a7fbd998c49a0a93583b9ff6187c5a96b49e584
siraj <siiraaj@yahoo.fr>
2018-04-23 21:51:04 +0200

    les cordonnees de la base de donnees

---
+++ WebServiceBoulot/WebServiceBoulot/DBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceBoulot
+{
+    public class DBConnection
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return "Data Source = 213.246.49.103; Initial Catalog = ajahot127969com28897_WebServiceTest; User ID = ajahot127969com28897_sirajTest; Password = 1imOk92%";
+
+            }
+
+
+        }
+    }
+}
